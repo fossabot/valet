4eee9568824d5e689fdea7d131c8b81b5a37ecac
jophde <jophde@gmail.com>
2014-04-09 21:25:39 -0700

    Make screen not stay on.

--- src/main/java/co/valetapp/activity/ParkActivity.java
+++ src/main/java/co/valetapp/activity/ParkActivity.java
@@ -370,8 +370,7 @@ public class ParkActivity extends FragmentActivity
         final Window win = getWindow();
         win.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                 | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-        win.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        win.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
     }
 
     private boolean isAlarmIntent() {
