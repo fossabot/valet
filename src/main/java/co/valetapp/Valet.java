bacca9169a767e59352b94d590b997b107e04f46
jophde <jophde@gmail.com>
2014-11-05 15:08:50 -0800

    Updates Android SDK

--- src/main/java/co/valetapp/Valet.java
+++ src/main/java/co/valetapp/Valet.java
@@ -1,16 +1,21 @@
 package co.valetapp;
 
 import android.app.Application;
+import android.content.Context;
+
 import com.colatris.sdk.Colatris;
 import com.crittercism.app.Crittercism;
-import com.taplytics.sdk.Taplytics;
 
 public class Valet extends Application {
+
     @Override public void onCreate() {
         super.onCreate();
 
-        Colatris.init(1, R.class, this, true);
+        Colatris.Config config = new Colatris.Config()
+                .setLogLevel(Colatris.Config.LogLevel.DEBUG)
+                .setSyncLevel(Colatris.Config.SyncLevel.NONE);
+        Colatris.init(1, R.class, this, config);
+
         Crittercism.initialize(getApplicationContext(), "5145fe5c4002050d07000002");
-//        Taplytics.startTaplytics(this, "e7a047d0103ff1d2e9af1c62d5419aa5c58b572d");
     }
 }
